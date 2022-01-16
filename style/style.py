faca03040f19a76e0051fa255e6d92676f28a843
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2022-01-16 03:56:55 -0600

    ✅ Adding: style QSS for login

---
+++ style/style.py
@@ -0,0 +1,59 @@
+style="""
+QFrame#frame1{
+    background: #E88317;
+    color: #000;
+}
+QFrame#frame2{
+    background: #2D3E52;
+    color: #000;
+}
+QFrame#frame3{
+    color: #000;
+}
+QLabel{
+  background: transparent;
+  color: #FFF;
+  font-weight: bold;
+  font-family: Arial;
+  font-size: 15px;
+}
+QLabel#label{
+  background: transparent;
+  color:#FFF;
+  text-decoration: blink;
+  font-variant: small-caps;
+  font-size: 25px;
+  text-align: center;  
+}
+QLineEdit{
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid #0F87D5;
+  font-size: 12px;
+  color:#fff;
+  font-family:sans-serif;
+}
+QCheckBox{
+  background: transparent;
+  border-bottom: none;
+  font-size: 12px;
+  color:#fff;
+  font-family:sans-serif;
+}
+QPushButton{
+  background: #E88317;
+  border-radius:20px;
+  color:#FFF;
+   padding: 12px 28px;
+  font-family: Arial;
+  font-size: 15px;
+}
+QPushButton:pressed {
+  background: #FF7C00;
+  border-radius:20px;
+  color:#FFF;
+   padding: 12px 28px;
+  font-family: Arial;
+  font-size: 15px;
+}
+"""
